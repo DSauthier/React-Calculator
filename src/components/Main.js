dea819b089d38919917aa45dca762123940f5a4f
DSauthier <danielsauthier@live.com>
2018-12-27 17:36:40 -0500

    added buttons

--- src/components/Main.js
+++ src/components/Main.js
@@ -12,10 +12,10 @@ render(){
       <div className="calc-wrapper">
        
         <div className="row">
-      <Button>7</Button>
-      <Button>8</Button>
-      <Button>9</Button>
-      <Button>/</Button>
+          <Button>7</Button>
+          <Button>8</Button>
+          <Button>9</Button>
+          <Button>/</Button>
         </div>
         <div className="row">
           <Button>4</Button>
@@ -27,7 +27,7 @@ render(){
           <Button>1</Button>
           <Button>2</Button>
           <Button>3</Button>
-          <Button>-</Button>
+          <Button>+</Button>
         </div>
         <div className="row">
           <Button>.</Button>
---
+++ src/components/input.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import './css/input.css';
+import { Button } from './Button'
+
+export const input = () =>{
+
+};
